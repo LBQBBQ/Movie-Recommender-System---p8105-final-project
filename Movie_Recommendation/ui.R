cb7c44da0abb4755523ce048188cb4597713f665
LBQBBQ <boqian.li@uconn.edu>
2021-12-04 13:58:00 -0500

    changes - Boqian

--- Movie_Recommendation/ui.R
+++ Movie_Recommendation/ui.R
@@ -23,15 +23,15 @@ library(shiny)
         titlePanel("Movie Recommender"),
         fluidRow(
           column(5,
-                 selectInput("Genre_1", label = "Genre 1",choices = "1"),
-                 selectInput("Genre_2", label = "Genre 2", choices = "2"),
-                 selectInput("Genre_3", label = "Genre 3", choices = "3"),
-                 submitButton("Click bottom to select movie you like"),
+                 selectInput("Genre_1", label = "Genre 1",choices = Genre),
+                 selectInput("Genre_2", label = "Genre 2", choices = Genre),
+                 selectInput("Genre_3", label = "Genre 3", choices = Genre),
+                 submitButton("Select movie you like"),
           column(5,
                  selectInput("movie_1", label = "Movie 1",choices = "1"),
                  selectInput("movie_2", label = "Movie 2", choices = "2"),
                  selectInput("movie_3", label = "Movie 3", choices = "3"),
-                 submitButton("Click bottom to get your movie recommendation"))
+                 submitButton("Get your movie recommendation"))
           )
         )
       )
